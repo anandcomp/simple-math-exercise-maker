c742b6075b227085fad9911ad680c22fdf197a18
Anand Ganesan <anandcs20@gmail.com>
2024-08-04 12:09:06 +0530

    added form to get user input

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -22,49 +22,101 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
+
 <head>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
+
 <body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+    <?php $this->beginBody() ?>
 
-<header id="header">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top mt-auto']
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'About', 'url' => ['/site/about']]
-        ]
-    ]);
-    NavBar::end();
-    ?>
-</header>
+    <header id="header">
+        <?php
+        NavBar::begin([
+            'brandLabel' => Yii::$app->name,
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => ['class' => 'navbar-expand-md navbar-light bg-light fixed-top mt-auto']
+        ]);
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav'],
+            'items' => [
+                // ['label' => 'About', 'url' => ['/site/about']]
+            ]
+        ]);
+        NavBar::end();
+        ?>
 
-<main id="main" class="flex-shrink-0" role="main">
-    <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
+    </header>
 
-<footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; agancode <?= date('Y') ?></div>
+    <main id="main" class="flex-shrink-0" role="main">
+        <div class="container">
+            <?php if (!empty($this->params['breadcrumbs'])) : ?>
+                <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
+            <?php endif ?>
+            <?= Alert::widget() ?>
+            <div class="row">
+                <div class="col-8"><?= $content ?></div>
+                <div class="col-4">
+                    <div class="container">
+                        <div class="row d-flex justify-content-center">
+                                <div class="card p-3 py-4">
+                                    <div class="text-center">
+                                        <img src="https://i.imgur.com/bDLhJiP.jpg" width="100" class="rounded-circle">
+                                    </div>
+
+                                    <div class="text-center mt-3">
+                                        <!-- <span class="bg-secondary p-1 px-4 rounded text-white">Pro</span> -->
+                                        <h5 class="mt-2 mb-0">Anand Ganesan</h5>
+                                        <span>Full stack software developer</span>
+
+                                        <div class="px-4 mt-1">
+                                            <p class="fonts">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+
+                                        </div>
+
+                                        <ul class="social-list">
+                                            <li><i class="fa fa-facebook"></i></li>
+                                            <li><i class="fa fa-dribbble"></i></li>
+                                            <li><i class="fa fa-instagram"></i></li>
+                                            <li><i class="fa fa-linkedin"></i></li>
+                                            <li><i class="fa fa-google"></i></li>
+                                        </ul>
+
+                                        <div class="buttons">
+
+                                            <button class="btn btn-outline-primary px-4">Message</button>
+                                            <button class="btn btn-primary px-4 ms-3">Contact</button>
+                                        </div>
+
+
+
+
+
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
-    </div>
-</footer>
+    </main>
 
-<?php $this->endBody() ?>
+    <footer id="footer" class="mt-auto py-3 bg-light">
+        <div class="container">
+            <div class="row text-muted">
+                <div class="col-md-6 text-center text-md-start">&copy; agancode <?= date('Y') ?></div>
+            </div>
+        </div>
+    </footer>
+
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>
